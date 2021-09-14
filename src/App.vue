dca870b05126bffd2d7b130694b6c99420bf2966
Kachang31 <faizal.urratori@gmail.com>
2021-09-14 20:14:44 +0700

    Baru

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,94 @@
 <script setup lang="ts">
-  import { reactive, ref } from 'vue'
+  import { onMounted, reactive, ref } from 'vue'
+  import Api from './services/api'
   type todoType = {
     id: number;
     nama: string;
     status: boolean;
   }
-  const todoList = reactive<todoType[]>([
-      { id: 1, nama: 'Belajar PHP', status: true },
-      { id: 2, nama: 'Makan', status: false }
-  ])
+  const todoList = reactive<todoType[]>([])
 
   const todo = ref('');
-  const addTodo = () => {
-    todoList.push({
-      id: 0, nama: todo.value, status: false
-    })
-    todo.value = '';
-  }
+  const idTodo = ref();
+  let isEdit = ref();
+  console.log(isEdit)
+  let indexTodo = ref();
 
+  
+  const addTodo = async () => {
+        const todoBody = {
+        id:'',
+        nama_task:todo.value,
+        status_task:2 ,
+      }
+    try{
+      const data = await Api.postResource('/task',todoBody,'POST')
+      todo.value = '';
+      
+            const response = await fetch('http://localhost:8181');
+            const sample = await response.json();
+            const lastArr =  sample.slice(-1).pop();
+            console.log(lastArr)
+
+                todoList.push({
+                  id: lastArr.id,
+                  nama: lastArr.nama_task,
+                  status: lastArr.status_task == 1 ? true : false
+                })
+      }
+    catch(err){
+    console.log(err)
+    }
+  }
+  
+  
   const editTodo = (i: number) => {
+    //  cekEdit = true ;
     const row = todoList[i];
     todo.value = row.nama; 
+    idTodo.value = row.id;
+    let indexId = indexTodo.value[i];
+    
+    isEdit.value = true ; 
+    console.log(isEdit.value)
+    return row ; 
   }
 
-  const deleteTodo = (i: number) =>{
-    todoList.splice(i, 1)
+  const simpanEdit = async (i:number) => {
+     
+     const todoBody = {
+        id:'',
+        nama_kegiatan:todo.value,
+        status:2 ,
+      }
+     const y = todoList[i]; 
+    
+     const path = '/task' + '/' + y.id; 
+     const data = await Api.putResource(path,todoBody,'PUT')
+     todoList.splice(0,todoList.length); 
+     const mintaData = await Api.getResource('/',todoList)
+     todo.value = '';
+     isEdit.value = false ; 
+     }
+
+  const deleteTodo = async (i: number) =>{
+    const index = todoList[i]; 
+    
+    const path = '/task' + '/' + index.id;
+    todoList.splice(0,todoList.length)
+    const data = await Api.deleteResource(path,'DELETE');
+    const get = await Api.getResource('/',todoList)
   }
+ 
+  onMounted(async() =>{
+    console.log(todoList)
+    try {
+      const data = await Api.getResource('/',todoList)
+    }
+    catch(err){
+      console.log(err)
+    }
+  })
 
 </script>
 
@@ -39,7 +102,7 @@
       <table class="table table-striped">
         <tr v-for="(todo, i) in todoList" :key="todo.id">
           <td>{{ i + 1 }}</td>
-          <td>{{ todo.nama }}</td>
+          <td><span :class="todo.status == true ? 'selesai' : 'blm-selesai'">{{ todo.nama }}</span></td>
           <td>
             <input type="checkbox" v-model="todo.status">
           </td>
@@ -54,12 +117,14 @@
 
       <div class="d-flex">
         <input type="text" v-model="todo" class="form-control">
-        <button type="button" class="btn btn-primary mr-2" @click="addTodo()">Simpan</button>
+        <button type="button" class="btn btn-primary mr-2" @click="isEdit = true ? simpanEdit() : addTodo() ">Simpan</button>
       </div>
 
       {{todoList}}
       <br>
       {{todo}}
+      <br>
+      {{isEdit}}
 
     </div>
   </div>
@@ -67,3 +132,8 @@
 
 </template>
 
+<style scoped>
+.selesai {
+  text-decoration: line-through;
+}
+</style>
